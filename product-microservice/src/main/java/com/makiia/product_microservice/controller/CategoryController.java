d4ffb1e357828c64fdd5abe1a9b468130368728f
RaulCudris <raulcudriz@hotmail.com>
2024-08-19 16:49:29 -0500

    Actualizacion del Config Services en el File Yaml

--- product-microservice/src/main/java/com/makiia/product_microservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/makiia/product_microservice/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.makiia.product_microservice.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Getter
 @Setter
 public class CategoryController {
-    @Value("${app.testProp")
+    @Value("${app.testProp}")
     private String testProp;
 
+    @GetMapping("test-prop")
     public String getTestProp() {
         return this.testProp;
     }
