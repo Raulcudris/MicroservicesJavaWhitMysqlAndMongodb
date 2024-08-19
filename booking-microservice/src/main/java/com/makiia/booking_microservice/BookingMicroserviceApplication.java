252f3bf5b85ea7b51c174264e8f19fe5c418d562
RaulCudris <raulcudriz@hotmail.com>
2024-08-19 11:40:00 -0500

    Actualizando proyecto con discovery-service

--- booking-microservice/src/main/java/com/makiia/booking_microservice/BookingMicroserviceApplication.java
+++ booking-microservice/src/main/java/com/makiia/booking_microservice/BookingMicroserviceApplication.java
@@ -1,8 +1,10 @@
 package com.makiia.booking_microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingMicroserviceApplication {
 
 	public static void main(String[] args) {
--- product-microservice/src/main/java/com/makiia/product_microservice/ProductMicroserviceApplication.java
+++ product-microservice/src/main/java/com/makiia/product_microservice/ProductMicroserviceApplication.java
@@ -1,9 +1,10 @@
 package com.makiia.product_microservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
